44d4f9dd6f0d1ac1a1358d0d27d2a3aa4a90e8a9
Hayou <hayouayana@gmail.com>
2025-10-15 01:17:26 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage/LandingPage"
+import SignUp from "./pages/Auth/SignUp"
+import Login from "./pages/Auth/Login"
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {/* public routes */}
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/singup" element={<SignUp/>} />
+          <Route path="/login" element={<Login/>} />
+
+          {/* catch all route */}
+          <Route path="*" element={<Navigate to="/" replace/>} />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+export default App
